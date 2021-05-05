52c820bc7a4ed15719e796dea3f5466b361bfc96
lumilitas <lucas23121995>
2021-05-05 14:00:27 -0300

    Configurando o debugger

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,8 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello Lucas' }),
-);
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  response.json({ message: 'Hello ' + name });
+});
 
 export default routes;
