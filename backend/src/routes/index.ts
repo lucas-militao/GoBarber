b3ee25c3bbdc6672ea8577b448d57dd6d691bd8a
lumilitas <lucas23121995>
2021-05-05 11:27:51 -0300

    Prettier integrado

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello Lucas' }));
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello Lucas' }),
+);
 
 export default routes;
