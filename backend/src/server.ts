897040bb53b9cc99a2b5eb12933493a5e91bba78
lumilitas <lucas23121995>
2021-05-05 11:19:36 -0300

    Configuração para importação de arquivos .ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => response.json({ message: "Hello Lucas" }));
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log("🎶SServer started on port 3333!");
+  console.log('🎶SServer started on port 3333!');
 });
