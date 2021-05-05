1f9f7ff86d2fd33edf0f544252f29a346b28108c
lumilitas <lucas23121995>
2021-05-05 16:20:13 -0300

    Implementado classe model e migration de User

--- backend/src/database/migrations/1620241503765-CreateUsers.ts
+++ backend/src/database/migrations/1620241503765-CreateUsers.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class createAppointments1620235071968 implements MigrationInterface {
+export class CreateUsers1620241503765 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -14,14 +14,17 @@ export class createAppointments1620235071968 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
-            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
