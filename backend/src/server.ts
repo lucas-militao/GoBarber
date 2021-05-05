888f4f80f8221e6aaa6e7ba454ffe6a0a5dfce7b
lumilitas <lucas23121995>
2021-05-05 10:59:51 -0300

    Eslint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Lucas" });
-});
+app.get("/", (request, response) => response.json({ message: "Hello Lucas" }));
 
 app.listen(3333, () => {
   console.log("🎶SServer started on port 3333!");
