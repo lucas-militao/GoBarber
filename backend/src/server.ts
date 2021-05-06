7937c32e55e87f4aa4413aeda82735b8547ad870
lumilitas <lucas23121995>
2021-05-06 14:32:03 -0300

    Implementada rota para visualização dos arquivos de forma estática

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,16 +3,14 @@ import 'reflect-metadata';
 import express from 'express';
 
 import './database';
-import appointmentsRouter from './routes/appointments.routes';
-import usersRouter from './routes/users.routes';
-import sessionsRouter from './routes/sessions.routes';
+import routes from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
-app.use('/appointments', appointmentsRouter);
-app.use('/sessions', sessionsRouter);
-app.use('/users', usersRouter);
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🎶SServer started on port 3333!');
