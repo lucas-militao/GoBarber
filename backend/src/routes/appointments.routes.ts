935e6218fd1ef1bcb917c1ac345b863f4af37c61
lumilitas <lucas23121995>
2021-05-06 09:56:21 -0300

    Implementada rota e regra de negócio de users

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -15,7 +15,7 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parseData = parseISO(date);
 
@@ -23,7 +23,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parseData,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,16 @@
 import 'reflect-metadata';
 
 import express from 'express';
-import routes from './routes';
 
 import './database';
 import appointmentsRouter from './routes/appointments.routes';
+import usersRouter from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
-app.use(appointmentsRouter);
+app.use('/appointments', appointmentsRouter);
+app.use('/users', usersRouter);
 
 app.listen(3333, () => {
   console.log('🎶SServer started on port 3333!');
