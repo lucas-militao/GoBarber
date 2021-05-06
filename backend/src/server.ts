deb0711ec904261b0762d5baef64bc3b738537ce
lumilitas <lucas23121995>
2021-05-06 10:43:10 -0300

    Criada a rota e regras de negócio de autenticação

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,11 +5,13 @@ import express from 'express';
 import './database';
 import appointmentsRouter from './routes/appointments.routes';
 import usersRouter from './routes/users.routes';
+import sessionsRouter from './routes/sessions.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/appointments', appointmentsRouter);
+app.use('/sessions', sessionsRouter);
 app.use('/users', usersRouter);
 
 app.listen(3333, () => {
