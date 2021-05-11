f04ff529a618247c104c416b59e298426f398b7e
lumilitas <lucas23121995>
2021-05-11 14:57:13 -0300

    Criado o componente e a estilização do Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,16 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );
