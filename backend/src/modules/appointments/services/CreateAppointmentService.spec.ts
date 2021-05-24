825c45ccd7225ff2ac4bf2394ded8d1d9fd0807d
lumilitas <lucas23121995>
2021-05-24 09:29:55 -0300

    Configurado o Coverage Report

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,7 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {});
 });
